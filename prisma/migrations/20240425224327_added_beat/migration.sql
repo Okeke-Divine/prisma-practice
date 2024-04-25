79e484f2d87ead5d6aa15fb0b31133772f83d1d9
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-26 00:02:00 +0100

    Major update

---
+++ prisma/migrations/20240425224327_added_beat/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Beat` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `genre` VARCHAR(191) NOT NULL,
+    `price` INTEGER NOT NULL,
+    `bpm` INTEGER NOT NULL,
+    `key` VARCHAR(191) NOT NULL,
+    `thumbnail` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
