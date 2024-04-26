3b27e7f2dfe98fd9f22e3d5644b0a078b1df236c
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-26 03:54:42 +0100

    Finished the Prisma cours by Web Dev Simplified

--- script.ts
+++ script.ts
@@ -2,30 +2,31 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.deleteMany();
-  const user = await prisma.user.create({
-    data: {
-      name: "Kyle",
-      email: "divine@gmail.com",
-      age: 27,
-      isAdmin: false,
-      role: "BASIC",
-      userPreference: {
-        create: {
-          emailUpdates: true,
-        },
-      },
-    },
-    select: {
-      name: true,
-      email: true,
-      userPreference: {
-        select: {
-          emailUpdates: true
-        }
-      }
-    }
-  });
+  // await prisma.user.create({
+  //   data: {
+  //     age:  20,
+  //     name: "Sally2",
+  //     email: "sally2@gmail.com",
+  //     role: "ADMIN",
+  //     isAdmin: true
+  //   }
+  // })
+  // const user = await prisma.user.update({
+  //   where: {
+  //     email: "therealkayks@gmail.com"
+  //   },
+  //   data: {
+  //     userPreference: {
+  //       disconnect: true
+  //     }
+  //   }
+  // })
+  // const user = await prisma.user.findFirst({
+  //   where: {
+  //     name: "Kayks"
+  //   }
+  // })
+  const user = await prisma.user.deleteMany()
   console.log(user);
 }
 
