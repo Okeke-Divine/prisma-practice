58947f0a698d8aa0d1f4d9831a96db0c86bb7ae8
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-26 03:16:00 +0100

    updates

--- script.ts
+++ script.ts
@@ -1,13 +1,31 @@
 import { PrismaClient } from "@prisma/client";
-
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.user.deleteMany();
   const user = await prisma.user.create({
     data: {
-        
+      name: "Kyle",
+      email: "divine@gmail.com",
+      age: 27,
+      isAdmin: false,
+      role: "BASIC",
+      userPreference: {
+        create: {
+          emailUpdates: true,
+        },
+      },
+    },
+    select: {
+      name: true,
+      email: true,
+      userPreference: {
+        select: {
+          emailUpdates: true
+        }
+      }
     }
-  })
+  });
   console.log(user);
 }
 
@@ -16,5 +34,5 @@ main()
     console.log(e.message);
   })
   .finally(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   });
